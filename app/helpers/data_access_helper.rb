0227a6fd901dec5f8cd0e5c4143efc61facc3947
Christoph Fabianek <christoph.fabianek@gmail.com>
2019-03-10 01:07:53 +0100

    filter for station

--- app/helpers/data_access_helper.rb
+++ app/helpers/data_access_helper.rb
@@ -3,7 +3,24 @@ module DataAccessHelper
         require 'open-uri'
         require 'csv'
 
-        open('https://www.zamg.ac.at/ogd/').string.force_encoding("utf-8").split("\n") rescue []
+        if (params["id"].to_s != "")
+        	response = open('https://www.zamg.ac.at/ogd/').string.force_encoding("utf-8") rescue nil
+        	if !response.nil?
+        		CSV.parse(response, headers: true, col_sep: ";").each do |row|
+        			if row["Station"].to_s == params["id"].to_s
+        				return [
+        					response.split("\n").first,
+        					row.to_csv(col_sep: ";").strip
+        				]
+        			end
+        		end
+        		[]
+        	else
+        		[]
+        	end
+        else 
+        	open('https://www.zamg.ac.at/ogd/').string.force_encoding("utf-8").split("\n") rescue []
+        end
 
     end
 end
